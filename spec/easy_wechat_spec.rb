8ecb38f3397f7e29d94acf05dab1e79e2b8e21d3
foobar <foobar@v2up.com>
2021-06-28 15:18:43 +0800

    first init

---
+++ spec/easy_wechat_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe EasyWechat do
+  it "has a version number" do
+    expect(EasyWechat::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
