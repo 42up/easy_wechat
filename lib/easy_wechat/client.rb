cb8abcf11f6e161b8f6cc1e4a0c5a24c241024fd
foobar <foobar@v2up.com>
2021-07-20 14:02:43 +0800

    fixed issue

--- lib/easy_wechat/client.rb
+++ lib/easy_wechat/client.rb
@@ -11,6 +11,7 @@ module EasyWechat
     ADD_NEWS_URL = "https://api.weixin.qq.com/cgi-bin/material/add_news"
     GET_USER_SUMMARY_URL = "https://api.weixin.qq.com/datacube/getusersummary"
     GET_USER_CUMULATE_URL = "https://api.weixin.qq.com/datacube/getusercumulate"
+    GET_TICKET_URL = "https://api.weixin.qq.com/cgi-bin/ticket/getticket"
 
     def initialize
     end
@@ -53,47 +54,54 @@ module EasyWechat
       r
     end
 
+    # 新增永久图文素材
+    # https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Adding_Permanent_Assets.html
+    def add_news(articles)
+      payload = { articles: articles }
+
+      resp = HTTPX.post(ADD_NEWS_URL, params: { access_token: access_token }, json: payload)
+
+      r = ::JSON.parse(resp.body, quirks_mode: true)
+      yield r if block_given?
+      r
+    end
+
+    def get_ticket(type)
+      resp = HTTPX.get(GET_TICKET_URL, params: { access_token: access_token, type: type })
+
+      r = ::JSON.parse(resp.body, quirks_mode: true)
+      yield r if block_given?
+      r
+    end
+
+    # 获取永久素材的列表
+    # https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Get_materials_list.html
+    def batchget_material(type = "image", offset = 0, count = 20)
+      # TODO: 检查参数完整性
+      # TODO: 检查参数合法性
+      payload = { type: type, offset: offset, count: count }
+
+      resp = HTTPX.post(BATCHGET_MATERIAL_URL, params: { access_token: access_token }, json: payload)
+
+      r = ::JSON.parse(resp.body, quirks_mode: true)
+      yield r if block_given?
+      r
+    end
+
     # 上传图文消息内的图片获取URL
     # https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Adding_Permanent_Assets.html
     def uploadimg(media)
       # https://honeyryderchuck.gitlab.io/httpx/wiki/Multipart-Uploads.html#notes
-      file = if media.is_a?(String) ? HTTP::FormData::File.new(media) : meida
-          resp = HTTPX.plugin(:multipart).post(UPLOADIMG_URL, params: { access_token: access_token }, form: { media: file })
+      file = media.is_a?(String) ? HTTP::FormData::File.new(media) : meida
+      resp = HTTPX.plugin(:multipart).post(UPLOADIMG_URL, params: { access_token: access_token }, form: { media: file })
 
-          r = ::JSON.parse(resp.body, quirks_mode: true)
-          yield r if block_given?
-          r
-        end
+      r = ::JSON.parse(resp.body, quirks_mode: true)
+      yield r if block_given?
+      r
+    end
 
-      # 获取永久素材的列表
-      # https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Get_materials_list.html
-      def batchget_material(type = "image", offset = 0, count = 20)
-        # TODO: 检查参数完整性
-        # TODO: 检查参数合法性
-        payload = { type: type, offset: offset, count: count }
-
-        resp = HTTPX.post(BATCHGET_MATERIAL_URL, params: { access_token: access_token }, json: payload)
-
-        r = ::JSON.parse(resp.body, quirks_mode: true)
-        yield r if block_given?
-        r
-      end
-
-      # 新增永久图文素材
-      # https://developers.weixin.qq.com/doc/offiaccount/Asset_Management/Adding_Permanent_Assets.html
-      def add_news(articles)
-        payload = { articles: articles }
-
-        resp = HTTPX.post(ADD_NEWS_URL, params: { access_token: access_token }, json: payload)
-
-        r = ::JSON.parse(resp.body, quirks_mode: true)
-        yield r if block_given?
-        r
-      end
-
-      def menu_create
-        raise NotImplementedError
-      end
+    def menu_create
+      raise NotImplementedError
     end
   end
 end
